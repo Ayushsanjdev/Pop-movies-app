b99ef96b6ab0046ac5e2884e7e074c83b697162d
Ayush Sanj <ayushsanjpro@gmail.com>
2021-05-10 16:01:19 +0530

    add: movie component to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Movie from './components/movie';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Movie />
+    </div>
   );
 }
 
