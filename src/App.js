80e357998f25651fbb84539bca97cca513c01f70
Ayush Sanj <ayushsanjpro@gmail.com>
2021-05-11 14:09:01 +0530

    add: movie api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Movie from './components/movie';
 
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?api_key=d52eaf3acfab66733baf520a2f96042a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setMovies(data.results);
+    });
+  }, [])
+
   return (
-    <div className="App">
-      <Movie />
+    <div className='App'>
+      <h1>Popcorn Time</h1>
+      {movies && movies.length > 0 && movies.map((movie) => 
+        <Movie key={movie.id} {...movie} />
+      )}
     </div>
   );
 }
--- src/components/movie.js
+++ src/components/movie.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const Movie = () => 
+const Movie = ({ title, poster_path, vote_average, vote_count }) => 
     <div className='movie'>
-        This is a movie component
+        <img src={"https://image.tmdb.org/t/p/w500" + poster_path } alt={title} />
+        <div>
+            { vote_average }
+        </div>
+        <div>
+            { vote_count }
+        </div>
     </div>;
 
 export default Movie;
