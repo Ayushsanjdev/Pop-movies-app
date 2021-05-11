49359246076522d3dba353e969d799f6dfef842f
Ayush Sanj <ayushsanjpro@gmail.com>
2021-05-11 19:52:59 +0530

    add: margin

--- src/components/movie.js
+++ src/components/movie.js
@@ -4,9 +4,9 @@ const Movie = ({ title, poster_path, vote_average, vote_count }) =>
     <div className='movie'>
         <img src={"https://image.tmdb.org/t/p/w500" + poster_path } alt={ title } />
         <div>
-          <div>{ vote_average } ⭐</div>
-          <div>{ vote_count }</div> 
+          <div>{ vote_average }⭐</div>
+          <div>({ vote_count })</div> 
         </div>
-    </div>;
+    </div>
 
 export default Movie;
