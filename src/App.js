19cfba1c26785a227f806d9a9bba110ccc0aca19
Ayush Sanj <ayushsanjpro@gmail.com>
2021-05-11 19:17:04 +0530

    add: css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Movie from './components/movie';
 
-const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?api_key=d52eaf3acfab66733baf520a2f96042a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?api_key=d52eaf3acfab66733baf520a2f96042a&language=en-US&sort_by=vote_count.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
 
 function App() {
 
@@ -19,10 +19,18 @@ function App() {
 
   return (
     <div className='App'>
-      <h1>Popcorn Time</h1>
-      {movies && movies.length > 0 && movies.map((movie) => 
+      <header>
+        <h1>
+          Popcorn Time 
+          <img src="https://img.icons8.com/cotton/64/000000/streaming-movies.png" alt='movie-icon'/>
+        </h1>
+      </header>
+      <hr />
+      <section>
+        {movies && movies.length > 0 && movies.map((movie) => 
         <Movie key={movie.id} {...movie} />
-      )}
+        )}
+      </section>
     </div>
   );
 }
--- src/components/movie.js
+++ src/components/movie.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 const Movie = ({ title, poster_path, vote_average, vote_count }) => 
     <div className='movie'>
-        <img src={"https://image.tmdb.org/t/p/w500" + poster_path } alt={title} />
+        <img src={"https://image.tmdb.org/t/p/w500" + poster_path } alt={ title } />
         <div>
-            { vote_average }
-        </div>
-        <div>
-            { vote_count }
+          <div>{ vote_average } ⭐</div>
+          <div>{ vote_count }</div> 
         </div>
     </div>;
 
