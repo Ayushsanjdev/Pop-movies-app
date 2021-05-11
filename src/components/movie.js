ced4388b970667787a338899a2f0e14565c0be00
Ayush Sanj <ayushsanjpro@gmail.com>
2021-05-11 22:06:40 +0530

    add: hover overview

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Movie = ({ title, poster_path, vote_average, vote_count }) => 
+const Movie = ({ title, poster_path, vote_average, vote_count, overview }) => 
     <div className='movie'>
         <img src={"https://image.tmdb.org/t/p/w500" + poster_path } alt={ title } />
         <div>
-          <div>{ vote_average }⭐</div>
-          <div>({ vote_count })</div> 
+          <div>{ title }</div>
+          <div>{ vote_average } ⭐ <br/>({ vote_count }) </div> 
+        </div>
+        <div className='movie-overview'>
+          { overview }
         </div>
     </div>
 
